8a4d93bab3473b4096266d8515e489435fbe5f69
Michael Greenly <mgreenly@gmail.com>
2017-04-20 13:35:03 -0500

    update ruby version

--- ruby/build.sh
+++ ruby/build.sh
@@ -12,22 +12,22 @@ docker pull mgreenly/debian:latest
 #
 # get the tag and id of the current image/tag 
 #
-oldtag=$(docker images --format="{{.ID}}:{{.Tag}}" mgreenly/ruby | grep -v latest | cut -f2 -d:)
-oldid=$(docker images --format="{{.ID}}:{{.Tag}}" mgreenly/ruby | grep latest | cut -f1 -d:)
+oldtag=$(docker images --format="{{.ID}}:{{.Tag}}" mgreenly/ruby2.4 | grep -v latest | cut -f2 -d:)
+oldid=$(docker images --format="{{.ID}}:{{.Tag}}" mgreenly/ruby2.4 | grep latest | cut -f1 -d:)
 
 #
 # generate the tag for the new image then build it and tag it 'latest'
 #
 newtag=$(date +"%Y%m%d%H%M%S")
-docker build --no-cache -t mgreenly/ruby:$newtag .
-docker tag mgreenly/ruby:"$newtag" mgreenly/ruby:latest
+docker build --no-cache -t mgreenly/ruby2.4:$newtag .
+docker tag mgreenly/ruby2.4:"$newtag" mgreenly/ruby2.4:latest
 
 if [[ -n "$oldtag" ]]; then
-  docker rmi mgreenly/ruby:$oldtag
+  docker rmi mgreenly/ruby2.4:$oldtag
 fi
 
 
 #
 # upload the new latest
 #
-docker push mgreenly/ruby:latest
+docker push mgreenly/ruby2.4:latest
