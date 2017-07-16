a0a2764da4fa409cdf8b575e5a3fd1bb89e3d2c1
Michael Greenly <mgreenly@gmail.com>
2017-07-16 09:02:18 -0500

    update alpine-ruby

--- alpine-ruby/README.md
+++ alpine-ruby/README.md
@@ -1,7 +1,7 @@
 # Alpine Ruby Base
 
 This image was derived from the official 'ruby-alpine' images.  The primary
-change was to move to Alpine 3.5 which uses LibreSSL instead of OpenSSL. it
+change was to move to Alpine 3.6 which uses LibreSSL instead of OpenSSL. it
 also removes the customized bundler configuration.
 
 This is a minimal image that does not include development tools or libraries
--- alpine-ruby/build.sh
+++ alpine-ruby/build.sh
@@ -5,49 +5,58 @@
 #
 set -u
 
-#
-# the image we're going to build
-#
-IMAGE_NAME="mgreenly/alpine-ruby"
+
 
 #
-# define the version info
+# define all the component versions used in building this image
 #
-export ALPINE_VERSION="3.5"
+export ALPINE_VERSION="3.6"
 export RUBY_MAJOR="2.4"
 export RUBY_VERSION="2.4.1"
 export RUBY_DOWNLOAD_SHA256="4fc8a9992de3e90191de369270ea4b6c1b171b7941743614cc50822ddc1fe654"
 export RUBYGEMS_VERSION="2.6.12"
-export BUNDLER_VERSION="1.14.6"
+export BUNDLER_VERSION="1.15.1"
+
+#
+# the images name/tag info
+#
+IMAGE_NAME="mgreenly/ruby"
+TIME=$(date +"%Y%m%d%H%M%S")
+IMAGE_TAG="${RUBY_VERSION}-alpine_${TIME}"
 
 #
 # display the version info
 #
+echo -e "\n${IMAGE_NAME}:${IMAGE_TAG}...\n"
+echo "      ALPINE_VERSION: $ALPINE_VERSION"
 echo "          RUBY_MAJOR: $RUBY_MAJOR"
 echo "        RUBY_VERSION: $RUBY_VERSION"
 echo "RUBY_DOWNLOAD_SHA256: $RUBY_DOWNLOAD_SHA256"
 echo "    RUBYGEMS_VERSION: $RUBYGEMS_VERSION"
 echo "     BUNDLER_VERSION: $BUNDLER_VERSION"
-source ../versions.sh
 
 #
-# then construct the Dockerfile
+# construct the docker file with this version info
 #
 envsubst < Dockerfile.tmpl > Dockerfile
 
+#
+# build the docker image
+#
 docker build \
-  -t "$IMAGE_NAME":"$RUBY_VERSION" \
+  --no-cache \
+  -t "$IMAGE_NAME":"$IMAGE_TAG" \
   .
 
 #
 # set the latest tag
 #
 docker tag \
-  "$IMAGE_NAME":"$RUBY_VERSION" \
+  "$IMAGE_NAME":"$IMAGE_TAG" \
   "$IMAGE_NAME":latest
 
 #
 # push the image
 #
-docker push "$IMAGE_NAME":"$RUBY_VERSION"
+docker push "$IMAGE_NAME":"$IMAGE_TAG"
 docker push "$IMAGE_NAME":latest
