b6e90fc28697b1c3721dcc254752e9b23683291f
Michael Greenly <michael.greenly@bestbuy.com>
2017-12-01 11:20:24 -0600

    update to current ruby/gems/bundler

--- alpine-ruby/build.sh
+++ alpine-ruby/build.sh
@@ -12,10 +12,10 @@ set -u
 #
 export ALPINE_VERSION="3.6"
 export RUBY_MAJOR="2.4"
-export RUBY_VERSION="2.4.1"
-export RUBY_DOWNLOAD_SHA256="4fc8a9992de3e90191de369270ea4b6c1b171b7941743614cc50822ddc1fe654"
-export RUBYGEMS_VERSION="2.6.12"
-export BUNDLER_VERSION="1.15.1"
+export RUBY_VERSION="2.4.2"
+export RUBY_DOWNLOAD_SHA256="748a8980d30141bd1a4124e11745bb105b436fb1890826e0d2b9ea31af27f735"
+export RUBYGEMS_VERSION="2.6.14"
+export BUNDLER_VERSION="1.16.0"
 
 #
 # the images name/tag info
@@ -34,6 +34,9 @@ echo "        RUBY_VERSION: $RUBY_VERSION"
 echo "RUBY_DOWNLOAD_SHA256: $RUBY_DOWNLOAD_SHA256"
 echo "    RUBYGEMS_VERSION: $RUBYGEMS_VERSION"
 echo "     BUNDLER_VERSION: $BUNDLER_VERSION"
+echo "          IMAGE_NAME: $IMAGE_NAME"
+echo "                TIME: $TIME"
+echo "           IMAGE_TAG: $IMAGE_TAG"
 
 #
 # construct the docker file with this version info
